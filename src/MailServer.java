520f1221e7d59f5d0125a28a4d6212e98e0aba8d
Fantomas4 <Fantomas4@users.noreply.github.com>
2018-12-04 19:29:46 +0200

    Worked on MailServer class and its inner RequestServiceThread class, made minor changes to the code structure.

--- src/MailServer.java
+++ src/MailServer.java
@@ -26,6 +26,28 @@ public class MailServer {
 
     }
 
+    private void handshakeListeningThread() {
+
+        while (true) {
+            try {
+                // accept an incoming handshake from a new client,
+                // and create a new socket for this particular client
+                Socket serviceSocket = serverSocket.accept();
+
+                // create a new RequestServiceThread object, pass it to a new serviceThread,
+                // start the serviceThread and then add
+                // it to the clientThreads list
+                Thread serviceThread = new Thread(new RequestServiceThread(serviceSocket));
+                serviceThread.start();
+                requestServiceThreads.add(serviceThread);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
     private class RequestServiceThread implements Runnable {
 
         private Socket reqSocket;
@@ -245,179 +267,161 @@ public class MailServer {
 
         }
 
-    }
+        public Account getUserAccount(String username) {
 
-    private void handshakeListeningThread() {
+            Account target = null;
 
-        while (true) {
-            try {
-                // accept an incoming handshake from a new client,
-                // and create a new socket for this particular client
-                Socket serviceSocket = serverSocket.accept();
-
-                // create a new RequestServiceThread object, pass it to a new serviceThread,
-                // start the serviceThread and then add
-                // it to the clientThreads list
-                Thread serviceThread = new Thread(new RequestServiceThread(serviceSocket));
-                serviceThread.start();
-                requestServiceThreads.add(serviceThread);
-            } catch (IOException e) {
-                e.printStackTrace();
+            for (Account account : accountList) {
+                if (account.getUsername().equals(username)) {
+                    target = account;
+                }
             }
 
+            return target;
         }
 
-    }
+        public String register(String username, String password) {
 
-    public Account getUserAccount(String username) {
-
-        Account target = null;
-
-        for (Account account : accountList) {
-            if (account.getUsername().equals(username)) {
-                target = account;
+            // Check whether the username already exists
+            for (Account account : accountList) {
+                if (account.getUsername().equals(username)) {
+                    // The given username is already taken (already exists)
+                    return "error_username_already_exists";
+                }
             }
+
+            // The given username is accepted, so a new
+            // account is created using the given username and password.
+            // The new account object is then added to the accountList array list.
+            accountList.add(new Account(username, password));
+
+            return "account_created_successfully";
         }
 
-        return target;
-    }
+        public String login(String givenUsername, String givenPassword) {
 
-    public String register(String username, String password) {
+            // Check whether an account with the given username exists
+            Account matchingAccount = null;
 
-        // Check whether the username already exists
-        for (Account account : accountList) {
-            if (account.getUsername().equals(username)) {
-                // The given username is already taken (already exists)
-                return "error_username_already_exists";
+            for (Account account : accountList) {
+                if (account.getUsername().equals(givenUsername)) {
+
+                    // An account with the target username exists
+                    matchingAccount = account;
+                }
             }
-        }
 
-        // The given username is accepted, so a new
-        // account is created using the given username and password.
-        // The new account object is then added to the accountList array list.
-        accountList.add(new Account(username, password));
+            if (matchingAccount != null) {
 
-        return "account_created_successfully";
-    }
+                // Check whether the given password is correct
+                boolean validPassword = matchingAccount.isCorrectPassword(givenPassword);
 
-    public String login(String givenUsername, String givenPassword) {
-
-        // Check whether an account with the given username exists
-        Account matchingAccount = null;
-
-        for (Account account : accountList) {
-            if (account.getUsername().equals(givenUsername)) {
-
-                // An account with the target username exists
-                matchingAccount = account;
-            }
-        }
-
-        if (matchingAccount != null) {
-
-            // Check whether the given password is correct
-            boolean validPassword = matchingAccount.isCorrectPassword(givenPassword);
-
-            if (validPassword) {
-                // The given password is valid
-                return "verification_success";
+                if (validPassword) {
+                    // The given password is valid
+                    return "verification_success";
+                } else {
+                    // The given password is invalid
+                    return "invalid_password";
+                }
             } else {
-                // The given password is invalid
-                return "invalid_password";
+                // matchingAccount is null, meaning no account exists with the given username
+                return "username_not_found";
             }
-        } else {
-            // matchingAccount is null, meaning no account exists with the given username
-            return "username_not_found";
+
         }
 
-    }
+        public String newEmail(String senderUsername, String receiverUsername, String subject, String mainBody) {
 
-    public String newEmail(String senderUsername, String receiverUsername, String subject, String mainBody) {
+            Account target = null;
 
-        Account target = null;
-
-        // check whether the receiver exists as a valid user
-        for (Account account : accountList) {
-            if (account.getUsername().equals(receiverUsername)) {
-                target = account;
-                break;
+            // check whether the receiver exists as a valid user
+            for (Account account : accountList) {
+                if (account.getUsername().equals(receiverUsername)) {
+                    target = account;
+                    break;
+                }
             }
-        }
 
-        if (target != null) {
-            // receiver is valid (username exists)
-            target.addNewEmail(new Email(senderUsername, receiverUsername, subject, mainBody));
-            return "email_sent_successfully";
-        } else {
-            // receiver is not valid (a user with the given username does not exist)
-            return "error_receiver_not_valid";
-        }
-
-    }
-
-    public ArrayList<String> getEmailsPreview(Account user) {
-        // replaces the showEmails function from the exercise description
-        ArrayList<String> emailDescriptions = new ArrayList<>();
-
-        int mailId = 1;
-        String newStatus;
-        String sender;
-        String subject;
-
-        for (Email mail : user.getMailbox()) {
-
-            if (mail.isNew()) {
-                newStatus = "[ New ]";
+            if (target != null) {
+                // receiver is valid (username exists)
+                target.addNewEmail(new Email(senderUsername, receiverUsername, subject, mainBody));
+                return "email_sent_successfully";
             } else {
-                newStatus = "       ";
+                // receiver is not valid (a user with the given username does not exist)
+                return "error_receiver_not_valid";
             }
 
-            sender = mail.getSender();
-            subject = mail.getSubject();
-
-            String result = mailId + "." + " " + newStatus + " " + sender + "      " + subject;
-            emailDescriptions.add(result);
-
-            mailId += 1;
         }
 
-        return emailDescriptions;
+        public ArrayList<String> getEmailsPreview(Account user) {
+            // replaces the showEmails function from the exercise description
+            ArrayList<String> emailDescriptions = new ArrayList<>();
 
-    }
+            int mailId = 1;
+            String newStatus;
+            String sender;
+            String subject;
 
-    public String getEmail(String emailId, Account user) {
-        // replaces the readEMail function from the exercise description
-        int targetId = Integer.parseInt(emailId);
+            for (Email mail : user.getMailbox()) {
 
-        if (targetId < 1 || targetId > user.getMailbox().size()) {
-            // invalid emailId given
-            return "error_invalid_emailId";
-        } else {
-            // the given emailId id valid
-            Email targetEmail = user.getMailbox().get(Integer.parseInt(emailId) - 1);
-            String result = targetEmail.getMainbody();
-            targetEmail.markAsRead();
+                if (mail.isNew()) {
+                    newStatus = "[ New ]";
+                } else {
+                    newStatus = "       ";
+                }
 
-            return result;
+                sender = mail.getSender();
+                subject = mail.getSubject();
+
+                String result = mailId + "." + " " + newStatus + " " + sender + "      " + subject;
+                emailDescriptions.add(result);
+
+                mailId += 1;
+            }
+
+            return emailDescriptions;
+
+        }
+
+        public String getEmail(String emailId, Account user) {
+            // replaces the readEMail function from the exercise description
+            int targetId = Integer.parseInt(emailId);
+
+            if (targetId < 1 || targetId > user.getMailbox().size()) {
+                // invalid emailId given
+                return "error_invalid_emailId";
+            } else {
+                // the given emailId id valid
+                Email targetEmail = user.getMailbox().get(Integer.parseInt(emailId) - 1);
+                String result = targetEmail.getMainbody();
+                targetEmail.markAsRead();
+
+                return result;
+            }
+
+        }
+
+        public String deleteEmail(String emailId, Account user) {
+            return user.deleteEmail(emailId);
+        }
+
+        public void logOut() {
+
+            // NOTE: outer class method directly
+            // accesses and changes inner class private variable
+
+            // log out the currently logged in user
+            loggedInUser = null;
+        }
+
+        public void exit() {
+            stopListening = true;
         }
 
     }
 
-    public String deleteEmail(String emailId, Account user) {
-        return user.deleteEmail(emailId);
-    }
 
-    public void logOut(RequestServiceThread userThread) {
 
-        // NOTE: outer class method directly
-        // accesses and changes inner class private variable
 
-        // log out the currently logged in user
-        loggedInUser = null;
-    }
-
-    public void exit() {
-
-    }
 
 }
